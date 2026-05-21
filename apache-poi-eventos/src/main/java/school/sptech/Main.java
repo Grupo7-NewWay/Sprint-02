9823dd00a3c059a51c6346983358bc3bb619a2b3
LarissaOkamoto <larissa.okamoto@sptech.school>
2026-05-21 19:26:49 -0300

    Atualização do Java - CLass Main

--- apache-poi-eventos/src/main/java/school/sptech/Main.java
+++ apache-poi-eventos/src/main/java/school/sptech/Main.java
@@ -31,6 +31,17 @@ public class Main {
             LeitorExcel leitorExcel = new LeitorExcel();
             List<Eventos> eventosExtraidos = leitorExcel.extrairEventos(caminho1);
             List<Chegadas> chegadasExtraidos = leitorExcel.extrairChegadas(caminho2);
+            List<Fonte> fonteExtraidos = leitorExcel.extrairFonte(caminho2);
+            List<Gasto> gastoExtraidos = leitorExcel.extrairGasto(caminho2);
+            List<Grupo> grupoExtraidos = leitorExcel.extrairGrupo(caminho2);
+            List<GrupoIdade> grupoIdadeExtraidos = leitorExcel.extrairGrupoIdade(caminho2);
+            List<Hospedagem> hospedagemExtraidos = leitorExcel.extrairHospedagem(caminho2);
+            List<Lazer> lazerExtraidos = leitorExcel.extrairLazer(caminho2);
+            List<Localizacao> localizacaoExtraidos = leitorExcel.extrairLocalizacao(caminho2);
+            List<Motivo> motivoExtraidos = leitorExcel.extrairMotivo(caminho2);
+            List<Pacotes> pacotesExtraidos = leitorExcel.extrairPacotes(caminho2);
+            List<Permanencia> permanenciaExtraidos = leitorExcel.extrairPermanencia(caminho2);
+            List<ServicoAgencia> servicoAgenciaExtraidos = leitorExcel.extrairServicoAgencia(caminho2);
 
             AwsCredentialsProvider credentials = DefaultCredentialsProvider.create();
 
@@ -61,7 +72,7 @@ public class Main {
             s3Client.putObject(
                     PutObjectRequest.builder()
                             .bucket(bucketName)
-                            .key("perfil.xlsx")
+                            .key("demanda_turistica_2021.xlsx")
                             .build(),
                     RequestBody.fromFile(filePath2)
             );
