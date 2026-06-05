c6a620daad88b66e0ba6493d4f6844ccc49789b5
iwakenzo <gustavo.iwahashi@sptech.school>
2026-06-05 19:27:08 -0300

    fix: java

--- java/apache-poi-eventos/src/main/java/school/sptech/Main.java
+++ java/apache-poi-eventos/src/main/java/school/sptech/Main.java
@@ -1,115 +1,55 @@
 package school.sptech;
 
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class Main {
 
-                    public static void main(String[] args) throws IOException {
-                            if (args.length < 3) {
-                                    throw new RuntimeException(
-                                            "Informe os três arquivos de Excel"
-                                    );
-                            }
+        public static void main(String[] args) throws IOException {
 
-                            String caminhoEventos = args[0];
-                            String caminhoDemanda = args[1];
-                            String caminhoChegadas = args[2];
+                String bucketName = System.getenv("S3_BUCKET");
+                String localPath = "/app/";
 
+                S3Provider provider = new S3Provider();
+                S3Client s3Client = provider.getS3Client();
 
-                            S3Provider provider = new S3Provider();
+                // Baixa os 3 arquivos do S3
+                s3Client.getObject(
+                        GetObjectRequest.builder()
+                                .bucket(bucketName)
+                                .key("eventos_2026.xlsx")
+                                .build(),
+                        Paths.get(localPath + "eventos_2026.xlsx")
+                );
 
-                            S3Client s3Client =
-                                    provider.getS3Client();
+                s3Client.getObject(
+                        GetObjectRequest.builder()
+                                .bucket(bucketName)
+                                .key("demanda_turistica_2021.xlsx")
+                                .build(),
+                        Paths.get(localPath + "demanda_turistica_2021.xlsx")
+                );
 
-                            String bucketName = "new-way-s3";
+                s3Client.getObject(
+                        GetObjectRequest.builder()
+                                .bucket(bucketName)
+                                .key("chegada_turistas.xlsx")
+                                .build(),
+                        Paths.get(localPath + "chegada_turistas.xlsx")
+                );
 
+                System.out.println("Download finalizado!");
 
-                            try {
-                                    s3Client.createBucket(
-                                            CreateBucketRequest.builder()
-                                                    .bucket(bucketName)
-                                                    .build()
-                                    );
+                LeitorExcel leitorExcel = new LeitorExcel();
+                leitorExcel.extrairEventos();
+                leitorExcel.extrairDemandaTuristica();
+                leitorExcel.extrairChegadas();
 
-                            } catch (Exception e) {
+                s3Client.close();
 
-                                    System.out.println(
-                                            "Bucket já existe"
-                                    );
-                            }
-
-                            Path filePathEventos =
-                                    Paths.get(caminhoEventos);
-
-                            s3Client.putObject(
-
-                                    PutObjectRequest.builder()
-                                            .bucket(bucketName)
-                                            .key("eventos_2026.xlsx")
-                                            .build(),
-
-                                    RequestBody.fromFile(
-                                            filePathEventos
-                                    )
-                            );
-
-                            Path filePathDemanda =
-                                    Paths.get(caminhoDemanda);
-
-                            s3Client.putObject(
-
-                                    PutObjectRequest.builder()
-                                            .bucket(bucketName)
-                                            .key("demanda_turistica_2021.xlsx")
-                                            .build(),
-
-                                    RequestBody.fromFile(
-                                            filePathDemanda
-                                    )
-                            );
-
-
-                            Path filePathChegadas =
-                                    Paths.get(caminhoChegadas);
-
-                            s3Client.putObject(
-
-                                    PutObjectRequest.builder()
-                                            .bucket(bucketName)
-                                            .key("chegada_turistas.xlsx")
-                                            .build(),
-
-                                    RequestBody.fromFile(
-                                            filePathChegadas
-                                    )
-                            );
-
-                            System.out.println(
-                                    "Upload finalizado com sucesso!"
-                            );
-
-                            LeitorExcel leitorExcel =
-                                    new LeitorExcel();
-
-                            leitorExcel.extrairEventos();
-
-                            leitorExcel.extrairDemandaTuristica();
-
-                            leitorExcel.extrairChegadas();
-
-                            s3Client.close();
-
-                            System.out.println(
-                                    "Processo finalizado com sucesso!"
-                            );
-                    }
+                System.out.println("Processo finalizado com sucesso!");
+        }
 }
